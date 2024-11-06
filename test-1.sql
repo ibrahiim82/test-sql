bd64ae29ae0b6566e0e177003968853bd7020bee
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-11-06 23:26:18 +0300

    lastcommit

--- test-1.sql
+++ test-1.sql
@@ -33,3 +33,141 @@ select 1 as one; --! Piyasa standartı olarak küçük yazmak uygun değildir.
 -- SELECT 1+2*4 AS toplam;
 -- SELECT AlbumId AS no,Title AS baslik FROM Album;
 SELECT AlbumId+120 AS no,Title AS baslik FROM Album;
+
+
+
+-- SELECT Album.AlbumId, Album.Title FROM Album;
+-- SELECT a.AlbumId, a.Title FROM Album AS a; --Tablo isimlendirme
+-- SELECT a.AlbumId AS Numara, a.Title AS baslik FROM Album AS a; --Tablo ve sütun isimlendirme
+-- SELECT a.AlbumId Numara, a.Title baslik FROM Album a; -- AS yazmadan(bir boşluk) bırakarak isimlendirme yapılabilir(tercih edilen yöntem).
+
+-- *DISTINCT -- Tekrar edilen kayıtların sadece bir defa gelmesini sağlar.(benzer kaydı engeller)
+-- SELECT DISTINCT City FROM Customer;
+-- SELECT DISTINCT Country FROM Customer;
+-- SELECT DISTINCT Country FROM Customer; --Bütun sütünlardaki verilerin aynı olduğunu kontrol eder.
+
+-- *WHERE -- Filtreleme
+-- SELECT * FROM Customer WHERE Country = 'USA'; --Eşit olanları getir
+-- SELECT * FROM Customer WHERE Country != 'USA'; --Eşit olmayanları getir(!=)(Tercih edilen yöntem)
+-- SELECT * FROM Customer WHERE Country <> 'USA'; --Eşit olmayanları getir(<>)
+-- SELECT * FROM Customer WHERE CustomerId >20; --Büyük olanları getir.
+-- SELECT * FROM Customer WHERE CustomerId >=20; --Büyük ve eşit olanları getir.
+-- SELECT * FROM Customer WHERE CustomerId <20; --Küçük olanları getir.
+-- SELECT * FROM Customer WHERE CustomerId <20; --Küçük ve eşit olanları getir.
+-- SELECT * FROM Customer WHERE CustomerId BETWEEN 10 AND 20; --10 ile 20 arasındaki kayıtları getir.(her ikisi de dahil)
+-- SELECT * FROM Customer WHERE CustomerId BETWEEN '2024-01-01' AND '2024-02-28'; --(genelde tarih filtrelemede kullanılır) 
+
+-- * WHERE * AND / OR / NOT
+-- SELECT * FROM Customer WHERE NOT Country = 'USA'; -- NOT operatörü genelde True/False (Null/0/'') verilerinde kullanılır. (WHERE isActive = FALSE) = (WHERE NOT isActive)
+-- SELECT * FROM Customer WHERE Country = 'USA' AND Company NOT NULL;
+-- SELECT * FROM Customer WHERE Country = 'USA' AND NOT Company; -- Alternatif NOT kullanımı.
+-- SELECT * FROM Customer WHERE Country = 'USA' OR Country = 'Brazil' OR Country = 'Denmark';
+-- SELECT * FROM Customer WHERE (Country = 'USA' OR Country = 'Brazil' OR Country = 'Denmark') AND CustomerId < 20;
+-- SELECT * FROM Customer WHERE (Country = 'USA' OR Country = 'Brazil' OR Country = 'Denmark') AND CustomerId < 20 AND State = 'SP';
+
+-- * WHERE * IN / NOT IN
+-- SELECT * FROM Customer WHERE Country IN ('USA', 'Brazil', 'Denmark');
+-- SELECT * FROM Customer WHERE Country NOT IN ('USA', 'Brazil', 'Denmark');
+-- SELECT * FROM Customer WHERE CustomerId IN (1,2,4,8,16,32);
+
+-- * WHERE * LIKE (SpecialChars:'%', JokerChar;'_' = SingleChar) -- Arama yapar.
+-- SELECT * FROM Customer WHERE Country LIKE 'USA'; -- 'USA' olanlar.
+-- SELECT * FROM Customer WHERE Address LIKE '696'; -- '696' ile başlayanlar.
+-- SELECT * FROM Customer WHERE Address LIKE '%langer'; -- 'langer' ile bitenler.
+-- SELECT * FROM Customer WHERE Address LIKE '%rue%'; -- içinde 'rue' geçenler.
+-- SELECT * FROM Customer WHERE Phone LIKE '_55_%' -- 2. ve 3. karakteri '5' olanları ve en az 4 karakter olanları getir.
+-- SELECT * FROM Customer WHERE Address LIKE '_a_%'; --2.karakter 'a' ve en az 3 karakter olanlar.
+-- SELECT * FROM Customer WHERE Phone LIKE '+__ 030%'; -- Ülke kodu bilinmeyen 030 ile başlaya telefonlar.
+-- SELECT * FROM Customer WHERE Phone LIKE '+__ 030%' AND FirstName = 'Niklas'; -- Niklas isimli 030 ile başlayan numaralı kayıtlar.
+
+-- * ORDER BY -- Sıralama (ASC = A'dan Z'ye - DESC = Z'den A'ya)
+-- SELECT * FROM Customer ORDER BY CustomerId DESC;
+-- SELECT * FROM Customer ORDER BY Country ASC; -- A-Z sırala
+-- SELECT * FROM Customer ORDER BY Country ASC; -- Z-A sırala
+-- SELECT * FROM Customer ORDER BY Country DESC, City ASC,LastName DESC; -- Sırasıyla Ülke-Şehir-Soyisim sıralaması yapar.
+
+
+
+
+--- --- --- JOINS --- --- ---
+
+--Birden fazla tablodan veri çekmek için kullanılır.
+
+-- * INNER JOIN -- Yalnızca kesişen kayıtları getirir.
+-- * (Alternatif yazım yöntemi olarak INNER JOIN yazmak yerine sadece JOIN yazılabilir.)(Default JOIN yöntemidir)(Piyasada kullanım:INNER JOIN)
+
+/*
+SELECT *
+FROM Artist AS art
+JOIN Album AS alb ON alb.ArtistId = art.ArtistId -- 'JOIN' == 'INNER JOIN'
+ORDER BY alb.ArtistId,alb.AlbumId
+*/
+
+/*
+SELECT c.FirstName, c.LastName, c.Country, i.InvoiceId, i.InvoiceDate, i.Total AS InvoiceTotal
+FROM Customer AS c
+INNER JOIN Invoice AS i ON i.CustomerId = c.CustomerId
+ORDER BY c.CustomerId
+*/
+
+/*
+SELECT t.Name Sarki, a.Title Album, m.Name Dosya, g.Name Tur
+FROM Track t
+INNER JOIN Album a ON a.AlbumId=t.AlbumId --eşittirin sağında veya solunda olması önemsizdir
+INNER JOIN MediaType m ON t.MediaTypeId=m.MediaTypeId
+INNER JOIN Genre g ON g.GenreID=t.GenreId
+*/
+
+
+-- * LEFT JOIN --Üstteki(FROM) tablodaki bütün kayıtları getir ve JOIN tablosundaki KESİŞEN kayıtları getir.
+
+/*
+SELECT *
+FROM Artist AS art
+LEFT JOIN Album AS alb ON alb.ArtistId=art.ArtistId --Eğer bir karşılığı yoksa NULL yazar.
+ORDER BY ArtistId ASC, AlbumId ASC
+*/
+
+
+-- * RIGHT JOIN -- Üst (FROM) tablodaki KESİŞEN kayıtlar ve JOIN tablodaki BÜTÜN kayıtları getir.
+/*
+SELECT *
+FROM Artist AS art
+RIGHT JOIN Album AS alb ON alb.ArtistId=art.ArtistId
+ORDER BY ArtistId ASC, AlbumId ASC
+*/
+
+
+-- * FULL OUTER JOIN -- Her iki tablonun BÜTÜN kayıtlarını göster, Eşleşenleri yanyana göster.
+/*
+SELECT *
+FROM Artist AS art
+FULL OUTER JOIN Album AS alb ON alb.ArtistId=art.ArtistId
+ORDER BY ArtistId ASC, AlbumId ASC
+*/
+
+
+-- * CROSS JOIN -- Her iki tablonun BÜTÜN kayıtlarını göster, İlişki gözetme.--(KULLANILAN BİR YÖNTEM DEĞİLDİR)
+/*
+SELECT *
+FROM Artist AS art
+CROSS JOIN Album AS alb
+ORDER BY ArtistId ASC, AlbumId ASC
+*/
+
+
+-- * JOIN ÖRNEKLER
+/*
+-- Hangi sanatçı hangi albümleri çıkarmıştır. Bir albüme sahip olmayan sanatçıları gösterme. Sadece albüm sahibi olan sanatçıları göster.
+SELECT t1.ArtistId, t1.Name AS sanatci, t2.Title AS album
+FROM Artist AS t1
+INNER JOIN Album AS t2 ON t1.ArtistId=t2.ArtistId
+-- WHERE t1.Name = 'Led Zeppeli'
+ORDER BY t1.ArtistId
+/*
+-- Bütün sanatçıları göster. Hangi sanatçı hangi albüme sahip onu da göster. Ama albüm sahibi olmayan kayıtlara NULL yaz.
+SELECT t1.ArtistId, t1.Name AS sanatci, t2.Title AS album
+FROM Artist AS t1
+LEFT JOIN Album AS t2 ON t2.ArtistId=t1.ArtistId
+ORDER BY t1.ArtistId
+*/
