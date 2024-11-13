9b70f45d7397125e3b3f06e18b93b8849ce93588
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-11-13 20:10:26 +0300

    lastcommit

---
+++ notlar.js
@@ -0,0 +1,26 @@
+/*
+// use strict ne işe yarar?
+
+JavaScript'te "use strict"; ifadesi, bir kod parçasının "strict mode"da çalışmasını sağlar. Strict mode, JavaScript'in daha güvenli ve hataya dayanıklı çalışmasına yardımcı olmak için bazı kısıtlamalar getirir. Bu mod, hatalı ya da kötü yazılmış kodu daha erken tespit etmeye yardımcı olabilir. Ayrıca, eski JavaScript özelliklerini ve yanlış kullanımları engeller.
+
+Strict mode’un sağladığı bazı avantajlar şunlardır:
+
+Değişkenlerin Tanımlanması: use strict kullanıldığında, değişkenler tanımlanmadan kullanılmaya çalışıldığında hata oluşur. 
+
+Örneğin:
+"use strict";
+x = 10; // Hata verir, çünkü 'x' tanımlanmadı
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
