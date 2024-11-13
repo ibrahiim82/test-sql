daa3fbf1f918cabeca4a8df15927e1ad1dda7575
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-11-13 20:24:03 +0300

    lastcommit

--- oop1.js
+++ oop1.js
@@ -1,5 +1,26 @@
 "use strict"
 
 /* -------------------------------------------------------
+    OOP --> Object Oriented Programming
+            (Nesneye Yönelik Programlama)
+------------------------------------------------------- */
+
+
+/* -------------------------------------------------------
     OBJECTS
 ------------------------------------------------------- */
+
+// JS'de direkt obje oluşturabiliriz.(Class'a ihtiyaç duymadan)
+// Obje isimlendirmede PascalCase veya camelCase isimlendirme yapabiliriz.
+
+const exampleObject ={
+    propertyName: 'value', //obje içindeki değişkene property,attribute,field denir.
+
+    methodName: function(){ //obje içinde tanımlanan fonksiyonlara method denir.
+        return "bu bir method'dur"
+    },
+
+    methodNameAlternative(){
+        return "bu da bir method'dur"
+    },
+}
