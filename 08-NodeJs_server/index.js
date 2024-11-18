501471ab04f37aec33a399bcd06cf52236ff373d
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-11-18 21:02:44 +0300

    lastcommit

--- 08-NodeJs_server/index.js
+++ 08-NodeJs_server/index.js
@@ -2,7 +2,7 @@
 
 // console.log('nodejs server');
 
-const http = require('node:http')
+const http = require("node:http");
 
 // http.createServer((request,response)=>{
 
@@ -16,12 +16,23 @@ const http = require('node:http')
 // })
 
 // gelen istekleri dinlemeye başla
-const app = http.createServer((req,res)=>{
-    console.log('server içerisindesin');
+// const app = http.createServer((req,res)=>{
+//     console.log('server içerisindesin');
 
-    res.end('hello Node')
-})
-app.listen(8000,'127.0.0.1')
+//     res.end('<h1> hello Node </h1>')
+// })
+// app.listen(8000,'127.0.0.1')
 
+// const app = http.createServer((req,res)=>{
+//     console.log('server içerisindesin');
 
+//     res.end('<h1> hello Node </h1>')
+// }).listen(8000,'127.0.0.1')
+//app.listen veya direkt listen diyebiliriz
 
+const app = http
+  .createServer((req, res) => {
+    res.end("<h1> hello Node </h1>");
+    console.log("server içerisindesin");
+  })
+  .listen(8000, "127.0.0.1");
