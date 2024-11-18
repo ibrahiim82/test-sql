89ac013fd910ae56988fa62f46080705891738e2
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-11-18 21:44:58 +0300

    lastcommit

--- 08-NodeJs_server/index.js
+++ 08-NodeJs_server/index.js
@@ -30,9 +30,38 @@ const http = require("node:http");
 // }).listen(8000,'127.0.0.1')
 //app.listen veya direkt listen diyebiliriz
 
-const app = http
-  .createServer((req, res) => {
-    res.end("<h1> hello Node </h1>");
-    console.log("server içerisindesin");
-  })
-  .listen(8000, "127.0.0.1");
+// const app = http.createServer((req, res) => {
+//     res.end("<h1> hello Node </h1>");
+//     console.log("server içerisindesin");
+//   }).listen(8000, "127.0.0.1",()=> console.log('server Runned http://127.0.0.1:8000'));
+
+require('dotenv').config()
+const PORT=process.env.PORT || 8000
+const HOST=process.env.HOST || `127.0.0.1`
+
+// const app = http.createServer((req, res) => {
+
+//     if (req.url=='/')
+//         if(req.method=='GET') 
+//             res.end('<h1> Home page </h1>')
+//         else
+//             res.end('<h1> You can not POST </h1>')   
+//     else if(req.url=='/user')
+//         res.end('<h1> Home page </h1>')
+//     else
+//         res.end('<h1> Home page </h1>')
+    
+// }).listen(PORT,() =>console.log(`server Runned http://${HOST}:${PORT}`));
+
+
+const app = http.createServer((req, res) => {
+
+    
+    
+}).listen(PORT,() =>console.log(`server Runned http://${HOST}:${PORT}`));
+
+
+
+
+
+
