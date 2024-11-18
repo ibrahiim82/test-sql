13bfcd594762e64ffd1f668cb9a2bae74945b663
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-11-18 20:57:03 +0300

    lastcommit

--- 08-NodeJs_server/index.js
+++ 08-NodeJs_server/index.js
@@ -1,6 +1,27 @@
-'use-strict'
+"use-strict";
 
-console.log('nodejs server');
+// console.log('nodejs server');
+
+const http = require('node:http')
+
+// http.createServer((request,response)=>{
+
+// })
+
+// server oluştur
+// http.createServer((req,res)=>{
+//     console.log('server içerisindesin');
+
+//     res.end('hello Node')
+// })
+
+// gelen istekleri dinlemeye başla
+const app = http.createServer((req,res)=>{
+    console.log('server içerisindesin');
+
+    res.end('hello Node')
+})
+app.listen(8000,'127.0.0.1')
 
 
 
