73d4f08fe9e52e63336ed830a241c93a20c9b8cf
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-11-20 22:30:31 +0300

    lastcommit

--- 09--Express/app.js
+++ 09--Express/app.js
@@ -53,13 +53,35 @@ const PORT = process.env.PORT || 8000
 // })
 //& bütün bunlara routing deniyor
 
-app.all('/', (req, res)=> {
+// app.all('/', (req, res)=> {
 
-    res.send({
-        message: "run all"
-    })
+//     res.send({
+//         message: "run all"
+//     })
 
-}) //all yazdığımızda hepsi(get,put,post,delete) kullanılıryor TERCİH EDİLMEZ!!
+// }) //all yazdığımızda hepsi(get,put,post,delete) kullanılıyor TERCİH EDİLMEZ!!
+
+//^ app.route()
+// app.route('/route')
+//     .get((req,res) => {res.send({ message:'get'})})
+//     .post((req,res) => {res.send({ message:'post'})})
+//     .put((req,res) => {res.send({ message:'put'})})
+//     .delete((req,res) => {res.send({ message:'delete'})})
+
+/* ------------------------------------------------------- */
+//^ URL (path) options:
+
+//app.get('/', (req,res) => res.send('/ =  root {home}')) //anasayfa için / yeterlidir
+//app.get('/path', (req,res) => res.send('/path = path')) // sonraki slash (/) önemsizdir
+//& express-url supported JokerChars:
+//app.get('/abc(x)?123', (req,res) => res.send('/abc(x)?123')) //abc123 ve abcx123 her ikiside çalışır. "?" olsada olur olmasada olur demek
+//app.get('/abc(x)+123', (req,res) => res.send('/abc(x)+123')) //abcx123 or abc....x123 x'ten (en az) bir veya daha fazla olacak tane olacak
+//app.get('/abc*123', (req,res) => res.send('/abc*123')) // abc123 or abc(ANY)123 yıldız işareti yerine herhangi birşeyi yazabiliriz hata vermez
+//& express-url supported RegExp: (RegularExpression)
+// app.get(/xyz/, (req,res) => res.send('/xyz/')) // içinde xyz olan path
+// app.get(/xyz$/, (req,res) => res.send('/xyz/')) // xyz ile biten path
+app.get(/^\/xyz/, (req,res) => res.send('/xyz/')) // xyz ile başlayan path. şapka işaretinden sonra slash koymak gerekir fakat slash bir özel karakter olduğu için kaçış operatörü (ters slash) da kullanmak gerekir.
+
 
 /* ------------------------------------------------------- */
 
