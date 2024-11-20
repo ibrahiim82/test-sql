c3ddab290c6dff4de6868d6695806343f487df63
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-11-20 23:27:04 +0300

    lastcommit

--- 09--Express/notlar.js
+++ 09--Express/notlar.js
@@ -9,10 +9,17 @@
 // CREATE = POST , READ = GET , UPDATE = PUT , DELETE = DELETE
 
 
+//* require
+// require, Node.js'de kullanılan bir fonksiyondur ve modül yüklemek için kullanılır. Modüller, dış kütüphaneleri, yerel dosyaları veya JSON verilerini yüklemeye yarar.
+// Node.js, JavaScript kodlarını modüller halinde organize etmenizi sağlar, ve require bu modülleri projeye dahil etmek için kullanılır.
 
+// const express = require('express'); // 'express' modülünü yüklüyoruz
+// const app = express();
+// Burada, require('express') ifadesi, Express.js kütüphanesini yüklüyor ve express değişkenine atıyor. Artık app nesnesi üzerinden Express'in sunduğu metodları kullanabilirsiniz.
 
-
-
+//* createServer
+// createServer, Node.js'in http veya https modülünde bulunan bir metodtur ve HTTP (veya HTTPS) sunucusu oluşturur.
+// Bu metodu kullanarak gelen HTTP isteklerini dinleyebilecek bir sunucu başlatırsınız
 
 
 
--- notlar3.js
+++ notlar3.js
@@ -7,9 +7,12 @@
 //* (MVC) model view controller nedir,ne işe yarar,model ne işe yarar,view ne işe yarar,controller ne işe yarar?
 
 
+//* require
+// require, Node.js'de modül (kütüphane veya dosya) yüklemek için kullanılan bir fonksiyondur. Node.js, JavaScript kodlarını modüller halinde organize etmenize olanak sağlar ve require, bu modülleri projenize dahil etmek için kullanılır.
 
-
-
+//* createServer
+// createServer, Node.js'in http veya https modülünde bulunan bir metodtur ve HTTP (veya HTTPS) sunucusu oluşturur.
+// Bu metodu kullanarak gelen HTTP isteklerini dinleyebilecek bir sunucu başlatırsınız
 
 
 
