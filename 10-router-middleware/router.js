e710c9535b6bba94b4df9a9cf38418bc7459e33f
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-11-21 20:20:41 +0300

    lastcommit

--- 10-router-middleware/router.js
+++ 10-router-middleware/router.js
@@ -1,7 +1,7 @@
 "use strict";
-/* -------------------------------------------------------
-    EXPRESSJS - ROUTING
-------------------------------------------------------- */
+
+//?    EXPRESSJS - ROUTING
+
 
 const express = require("express");
 const app = express();
@@ -10,13 +10,33 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
-//? "Router" is special app for URL control in ExpressJS.
+//& "Router" is special app for URL control in ExpressJS.
 
-app.get('/welcome', (req,res) => {
-    res.send({
-        message: 'welcome'
-    })
-})
+// app.get('/welcome', function(req,res) {
+//     res.send({
+//         message: 'welcome'
+//     })
+// })
+
+//^ express.Router()
+const router = express.Router()
+
+// router.get('/', (req,res) => { res.send({message: 'Welcome'})})
+// router.post('/', (req,res) => { res.send({message: 'Post'})})
+// router.put('/', (req,res) => { res.send({message: 'Put'})})
+// router.delete('/', (req,res) => { res.send({message: 'Delete'})})
+
+// router.all('/', (req,res) => { res.send({message: 'All'})})
+//& app ile yaptığımız bütün routing işlemlerini 'router' ile yapabliriz
+router.route('/')
+    .get((req,res) => { res.send({message: 'Welcome'})})
+    .post((req,res) => { res.send({message: 'Post'})})
+    .put((req,res) => { res.send({message: 'Put'})})
+    .delete((req,res) => { res.send({message: 'Delete'})})
+
+//& app'e router kullandığımı bildirmem gerek. (app'e router'ı aktar.)
+app.use(router)
+
 
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
