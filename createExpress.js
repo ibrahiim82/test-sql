16ca9c1c82f03ff2568ccf334b6fd27ab3dabd24
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-11-26 20:04:50 +0300

    firstcommit

---
+++ createExpress.js
@@ -0,0 +1,28 @@
+/*
+
+
+npm init -y
+
+npm install express
+
+/ Express'i dahil et
+const express = require('express');
+
+/ Uygulamayı oluştur
+const app = express();
+
+/ Anasayfa yönlendiricisi
+app.get('/', (req, res) => {
+  res.send('Merhaba, Express!');
+});
+
+/ Sunucuyu başlat
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Sunucu http://localhost:${PORT} adresinde çalışıyor`);
+});
+
+nodemon app.js
+
+
+*/
