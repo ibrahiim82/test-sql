fde6bd6cf95905efb6c9ec897d93231adff3dcba
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-11-28 22:24:43 +0300

    lastcommit

--- 13-todo-sequelize_2/app-original.js
+++ 13-todo-sequelize_2/app-original.js
@@ -6,7 +6,7 @@
 // $ npm init -y
 // $ npm i express dotenv express-async-errors
 // $ echo PORT=8000 > .env
-// $ npm i sequelize sqlite
+// $ npm i sequelize sqlite3
 
 
 const express = require("express");
