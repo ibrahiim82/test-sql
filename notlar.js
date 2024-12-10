1c3becc86ba0b06a29cb3d0e3093819574e70bb8
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-12-10 21:49:37 +0300

    lastcommit

--- notlar.js
+++ notlar.js
@@ -1,5 +1,5 @@
 /*
-//*  use strict ne işe yarar?
+*  use strict ne işe yarar?
 
 JavaScript'te "use strict"; ifadesi, bir kod parçasının "strict mode"da çalışmasını sağlar. Strict mode, JavaScript'in daha güvenli ve hataya dayanıklı çalışmasına yardımcı olmak için bazı kısıtlamalar getirir. Bu mod, hatalı ya da kötü yazılmış kodu daha erken tespit etmeye yardımcı olabilir. Ayrıca, eski JavaScript özelliklerini ve yanlış kullanımları engeller.
 
@@ -24,13 +24,13 @@ x = 10; // Hata verir, çünkü 'x' tanımlanmadı
 
 
 
-//   class fonksiyonlar ne işe yarar
+*class fonksiyonlar ne işe yarar
 
-//   JSON nedir neden lazımdır
+*JSON nedir neden lazımdır
+
+
+
 json: objenin string halidir.
-
-
-
 
 
 
