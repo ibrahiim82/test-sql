44ef561b97a77264f3ecc15a7fd21fc65a4a5ba1
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-12-11 21:02:21 +0300

    lastcommit

---
+++ 20-PersonnelApi-PermissionToken/src/routes/token.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const {isAdmin} = require("../middlewares/permissions")
+const router = require("express").Router()
+const token = require("../controllers/token")
+
+router.use(isAdmin)
+
+router.route("/").get(token.list).post(token.create)
+
+router.route("/:id")
+.get(token.read)
+.put(token.update)
+.patch(token.update)
+.delete(token.delete)
+
+module.exports = router
