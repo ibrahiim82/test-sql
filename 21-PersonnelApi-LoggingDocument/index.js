ca52e98ce562f48bd6eceeb17f1b6e063a99ea35
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-12-12 21:16:49 +0300

    lastcommit

--- 21-PersonnelApi-LoggingDocument/index.js
+++ 21-PersonnelApi-LoggingDocument/index.js
@@ -20,16 +20,40 @@ require("express-async-errors");
 /* ------------------------------------------------------- */
 //* LOGGER
 // npm i morgan
+// https://expressjs.com/en/resources/middleware/morgan.html
 
-// const morgan = require("morgan")
+const morgan = require("morgan")
 
 // Morgan bir middleware'dir
 // app.use(morgan("tiny"))
 // app.use(morgan("short"))
 // app.use(morgan("dev"))
 // app.use(morgan("common"))
-app.use(morgan("combined"))
+// app.use(morgan("combined"))
 
+// Custom Logs:
+// app.use(morgan('TIME=":date[iso]" - URL=":url" - Method=":method" - IP=":remote-addr" - Ref=":referrer" - Status=":status" - Sign=":user-agent" (:response-time[digits] ms)'))
+
+// Write to file - log kayıtlarını dosyaya yazır
+// https://nodejs.org/api/fs.html#file-system-flags
+// const fs = require('node:fs') //& iki nokta üst üste varsa npm i yapmaya gerek yok çünkü node'un içinde var olan paket demektir.
+
+// app.use(morgan("combined", {
+//   stream: fs.createWriteStream('./access.log', { flags: "a+" })
+// })) // kayıtları access.log isimli dosyaya sakla. // flags: dosyayı açma ve yazma modunu belirtir.
+
+//Write to file day-by-day:
+// const fs = require("node:fs")
+// const now = new Date()
+// console.log(now, typeof now);
+// const today = now.toISOString().split('T')[0]
+// console.log(today);
+// app.use(morgan("combined", {
+//   stream: fs.createWriteStream(`./logs/${today}.log`, { flags: 'a+' })
+// }))
+
+// Moved to file:
+app.use(require('./src/middlewares/logger'))
 
 /* ------------------------------------------------------- */
 //db connection
