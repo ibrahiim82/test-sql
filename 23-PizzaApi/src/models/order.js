07fd3771e82deac0525d5845f21c4052157636b7
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-12-16 21:04:28 +0300

    lastcommit

--- 23-PizzaApi/src/models/order.js
+++ 23-PizzaApi/src/models/order.js
@@ -5,9 +5,27 @@
 const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
 // Order Model:
-
+// {
+//   small:"Small"
+// }
+// Object.values ("obje ismi").length===0 // obje değeri boş mu dolumu kontrolü yapılır
 const OrderSchema = new mongoose.Schema(
-  {},
+  {
+    userId:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    pizzaId:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Pizza",
+    },
+    size: {
+      type: String,
+      trim: true,
+      required: true,
+      enum: ["Small", "Medium", "Large", "XLarge"] //& enum ile sadece bu dizideki değerlerden birisi olmak zorunda olduğunu belirttik
+    },
+  },
   {
     collection: "orders",
     timestamps: true,
--- 23-PizzaApi/src/models/pizza.js
+++ 23-PizzaApi/src/models/pizza.js
@@ -7,7 +7,26 @@ const { mongoose } = require("../configs/dbConnection");
 // Pizza Model:
 
 const PizzaSchema = new mongoose.Schema(
-  {},
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+
+    image: String,
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    toppingIds: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Topping",
+    }],
+  },
 
   {
     collection: "pizzas",
