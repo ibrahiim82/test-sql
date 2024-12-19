2c1f05be187554fb60c6d9cb12c1d9e6cc53c56c
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-12-19 21:30:44 +0300

    lastcommit

--- 25-PizzaApi - Email-Upload/src/routes/pizza.js
+++ 25-PizzaApi - Email-Upload/src/routes/pizza.js
@@ -8,11 +8,30 @@ const router = require('express').Router()
 
 const pizza = require('../controllers/pizza')
 
+/* ------------------------------------------------------- */
+//* UPLOADING - MULTER
+// npm i multer
+
+const multer = require('multer')
+
+const upload = multer({
+    // dest: './upload',
+    storage: multer.diskStorage({
+        destination: './upload',
+        // filename:
+    })
+})
+//& multer.diskstorage kullanmak sadece multer kullanmaktan daha kullanışlıdır örneğin oluşan dosyanın adı değiştirelebilir
+
+/* ------------------------------------------------------- */
 // URL: /pizzas
 
 router.route('/')
     .get(pizza.list)
-    .post(pizza.create)
+    // .post(pizza.create)
+    .post(upload.single('fieldName'),pizza.create)
+    // .post(upload.array('fieldName'),pizza.create)
+    // .post(upload.any('fieldName'),pizza.create)
 
 router.route('/:id')
     .get(pizza.read)
