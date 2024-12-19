75bdf9d0884f4df8c311cdd5bb58ba9c3933d3e6
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-12-19 20:50:50 +0300

    lastcommit

--- 25-PizzaApi - Email-Upload/index.js
+++ 25-PizzaApi - Email-Upload/index.js
@@ -48,14 +48,100 @@ app.use(require("./src/middlewares/queryHandler"));
 /* ------------------------------------------------------- */
 //* EMAIL - NODEMAILER
 // nmp i nodemailer
+// https://www.nodemailer.com/
 
+const nodemailer = require('nodemailer')
 
+// Create a new Test Account (fake mail)
+// nodemailer.createTestAccount().then((data => console.log(data)))
 
+/*
+{
+  {
+    user: 'dmrnbudxpsaxht6l@ethereal.email',
+    pass: 'rprDNQDE8VSVcexf8U',
+    smtp: { host: 'smtp.ethereal.email', port: 587, secure: false },
+    imap: { host: 'imap.ethereal.email', port: 993, secure: true },
+    pop3: { host: 'pop3.ethereal.email', port: 995, secure: true },
+    web: 'https://ethereal.email',
+    mxEnabled: false
+  }
+}
+*/
 
+// Connection to MailServer/SMTP:
+// const transporter = nodemailer.createTransport({
+//   / SMTP:
+//   host: 'smtp.ethereal.email',
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: 'dmrnbudxpsaxht6l@ethereal.email',
+//     pass: 'rprDNQDE8VSVcexf8U'
+//   }
+// })
+// / console.log(transporter);
 
+// / SendMail:
+// transporter.sendMail({
 
+//   from: "dmrnbudxpsaxht6l@ethereal.email", // from yazılması mecburi değil
+//   to: 'qadir@clarusway.com', // 'abc@xyz.com, def@xyz.com
+//   subject: 'Hello', // Mail başlığı
+//   text: 'Hello there. How are you?', // Mail içeriği (düz metin)
+//   html: '<h2>Hello there.</h2> <p>How are you?</p>' // Mail içeriği (html ile)
 
+// }, function(error, success) {// mail gönderme işlemi başarısızsa error dolu gelir succes boş gelir,başarılıysa error boş success dolu gelir
 
+//   success ? console.log('SUCCESS:', success) : console.log('ERROR:', error)
+
+// }) 
+
+//^ GoogleMail (gmail.com)
+//
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'yavuzyolcu82@gmail.com',
+//     pass: 'arla gpte akoz fcvj'
+//   }
+// })
+//& gönderen mail adresi yanlışsa mail spam kutusuna düşer
+// transporter.sendMail({
+//   from: 'yavuzyolcu@gmail.com', // tavsiye: gönderen mail adresinin from içinde belirtilmesi
+//   to: 'yavuzyolcu82@gmail.com', // 'abc@xyz.com, def@xyz.com
+//   subject: 'Hello', // Mail başlığı
+//   text: 'Hello there. How are you?', // Mail içeriği (düz metin)
+//   html: '<h2>Hello there.</h2> <p>How are you?</p>' // Mail içeriği (html ile)
+
+// }, function(error, success) {// mail gönderme işlemi başarısızsa error dolu gelir succes boş gelir,başarılıysa error boş success dolu gelir
+
+//   success ? console.log('SUCCESS:', success) : console.log('ERROR:', error)
+
+// }) 
+
+//^ YandexMail (yandex)
+// const transporter = nodemailer.createTransport({
+//     service: 'yandex',
+//     auth: {
+//         user: 'test@yandex.com',
+//         pass: '11' // your email-password
+//     }
+// })
+
+// transporter.sendMail({
+
+//   from: 'yavuzyolcu@gmail.com', // tavsiye: gönderen mail adresinin from içinde belirtilmesi
+//   to: 'yavuzyolcu82@gmail.com', // 'abc@xyz.com, def@xyz.com
+//   subject: 'Hello', // Mail başlığı
+//   text: 'Hello there. How are you?', // Mail içeriği (düz metin)
+//   html: '<h2>Hello there.</h2> <p>How are you?</p>' // Mail içeriği (html ile)
+
+// }, function(error, success) {// mail gönderme işlemi başarısızsa error dolu gelir succes boş gelir,başarılıysa error boş success dolu gelir
+
+//   success ? console.log('SUCCESS:', success) : console.log('ERROR:', error)
+
+// }) 
 
 /* ------------------------------------------------------- */
 // Routes:
