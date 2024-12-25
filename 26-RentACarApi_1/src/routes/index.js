3e5e14bd1278be63e3289230e03eb98e38f5f767
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-12-25 22:27:32 +0300

    lastcommit

--- 26-RentACarApi_1/src/routes/index.js
+++ 26-RentACarApi_1/src/routes/index.js
@@ -10,6 +10,7 @@ const router = require('express').Router()
 router.use('/auth', require('./auth'))
 router.use('/users', require('./user'))
 router.use('/cars', require('./car'))
+router.use('/tokens', require('./token'))
 router.use('/reseravtions', require('./reservation'))
 
 module.exports = router
