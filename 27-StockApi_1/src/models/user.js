49b068d99fa046aede834624143efc232f042f63
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-12-30 20:24:08 +0300

    lastcommit

--- 27-StockApi_1/src/models/user.js
+++ 27-StockApi_1/src/models/user.js
@@ -4,3 +4,61 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema({
+
+    username: {
+        type:String,
+        trim: true,
+        required:true,
+        unique:true,
+        index:true
+    },
+    
+    password: {
+        type:String,
+        trim:true,
+        required:true
+    },
+
+    email: {
+        type:String,
+        trim: true,
+        required:true,
+        unique:true,
+        index:true
+    },
+
+    firstName: {
+        type:String,
+        trim: true,
+        required:true,
+    },
+
+    lastName: {
+        type:String,
+        trim: true,
+        required:true,
+    },
+
+    isActive: {
+        type:Boolean,
+        default:true
+    },
+
+    isStaff: {
+        type:Boolean,
+        default:false
+    },
+
+    isAdmin: {
+        type:Boolean,
+        default:true
+    },
+
+},{
+    collection: 'users',
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', UserSchema)
