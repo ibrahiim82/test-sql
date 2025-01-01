f17e1053bc7f3d7663524eaa1e15d80b63db4656
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-01-01 16:49:03 +0300

    lastcommit

--- 26-RentACarApi_1/src/configs/dbConnection.js
+++ 26-RentACarApi_1/src/configs/dbConnection.js
@@ -4,17 +4,25 @@
 ------------------------------------------------------- */
 // MongoDB Connection:
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose')    //Mongoose modülü, mongoose adlı değişkene aktardık.
 
+//& Bağlantı Fonksiyonu Tanımlanması (dbConnection):
 const dbConnection = function() {
     // Connect:
     mongoose.connect(process.env.MONGODB)
         .then(() => console.log('* DB Connected * '))
         .catch((err) => console.log('* DB Not Connected * ', err))
 }
+//dbConnection fonksiyonu, MongoDB veritabanına bağlanmak için kullandık.
+
+// mongoose.connect(process.env.MONGODB): MongoDB'ye bağlantı kurmak için Mongoose'un connect metodunu çağırdık. Bağlantı için bağlantı dizesi (URL), process.env.MONGODB ifadesi ile çevresel değişkenlerden alınır. Bu genellikle uygulamanın çalıştığı ortamda (örneğin bir .env dosyasından) ayarlanır.
+
+// then: Bağlantı başarılı olduğunda * DB Connected * mesajı yazdırılır.
+// catch: Bağlantı hatalı olduğunda, hata mesajı konsola yazdırılır.
+
 
 /* ------------------------------------------------------- */
 module.exports = {
     mongoose,
     dbConnection
-} 
+} // Bu, başka dosyalarda bu dosyayı içe aktarma (import) işlemi yapabilmek için gereklidir. Burada, mongoose ve dbConnection fonksiyonunu dışa aktardık.
