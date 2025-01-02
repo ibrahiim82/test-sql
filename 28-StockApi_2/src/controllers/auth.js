0d1d4c5d193136bb90ab0c41bfdf881ce4374c6f
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-01-02 20:20:27 +0300

    up

--- 28-StockApi_2/src/controllers/auth.js
+++ 28-StockApi_2/src/controllers/auth.js
@@ -2,3 +2,34 @@
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
+
+const User = require('../models/user')
+const Token = require('../models/token')
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+const jwt = require('jsonwebtoken')
+
+module.exports = {
+    login: async () => {
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "Login"
+            #swagger.description = 'Login with username (or email) and password for get simpleToken and JWT'
+            #swagger.parameters["body"] = {
+                in: "body",
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                }
+            }
+        */
+    },
+
+    refresh: async () => {
+
+    },
+
+    logout: async () => {
+
+    }
+}
